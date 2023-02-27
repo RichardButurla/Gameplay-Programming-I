2afa532f5e3e37da7d2d24e84d2bcbc9056dfb1d
RichardButurla <rj.buturla@gmail.com>
2023-02-27 10:30:35 +0000

    added cube

---
+++ OpenGL3/SFMLOpenGL/Game.cpp
@@ -0,0 +1,288 @@
+#include <Game.h>
+
+Game::Game() : window(VideoMode(800, 600), "OpenGL")
+{
+	
+}
+
+Game::~Game() {}
+
+void Game::run()
+{
+
+	initialize();
+
+	Event event;
+
+	while (isRunning) {
+
+		cout << "Game running..." << endl;
+
+		while (window.pollEvent(event))
+		{
+			processEvents(event);
+		}
+		update();
+		draw();
+	}
+
+}
+
+void Game::initialize()
+{
+	isRunning = true;
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0, window.getSize().x /
+		window.getSize().y, 1.0, 500.0);
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void Game::update()
+{
+	
+}
+
+void Game::draw()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	
+		   
+	//glBegin(GL_TRIANGLE_FAN); {
+	//	// Set OpenGL Vertices
+	//	for (int i = 0; i < 6; i++)
+	//	{
+	//		glVertex3f(m_pyramidPoints[i].x, m_pyramidPoints[i].y, m_pyramidPoints[i].z);
+	//		glColor3f(m_pyramidColors[i].x, m_pyramidColors[i].y, m_pyramidColors[i].z);		
+	//	}
+	//}
+	//
+
+	glBegin(GL_QUADS); {
+
+		for (int i = 0; i < 6; i++)
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				glVertex3f(m_cubePoints[i][j].x, m_cubePoints[i][j].y, m_cubePoints[i][j].z);
+				glColor3f(m_cubeColors[j].x, m_cubeColors[j].y, m_cubeColors[j].z);
+			}
+		}
+	}
+	glEnd();
+	
+	
+
+	
+	
+	window.display();
+}
+
+void Game::processEvents(Event event)
+{
+	if (event.type == Event::Closed)
+	{
+		isRunning = false;
+	}
+	MyVector3 moveVector{ 0,0,0 };
+
+	switch (event.type)
+	{
+	case sf::Event::Closed:
+		window.close();
+		break;
+	case sf::Event::KeyPressed:
+		
+
+		MyMatrix xRotation;
+		MyMatrix yRotation;
+		MyMatrix zRotation;
+		
+		switch (event.key.code)
+		{
+		case sf::Keyboard::T:
+			yRotation = MyMatrix::rotationAntiClockwiseY(rotation);
+
+			for (int i = 0; i < 6; i++)
+			{
+				MyVector3 originVector{ MyVector3{0,0,0} - m_pyramidPoints[i] };
+				MyVector3 returningVector{ m_pyramidPoints[i] };
+
+				MyMatrix translateToOrigin = MyMatrix::translationXY(originVector);
+				MyMatrix translateBack = MyMatrix::translationXY(returningVector);
+
+				//m_pyramidPoints[i] = yRotation * m_pyramidPoints[i] ;
+
+				for (int i = 0; i < 6; i++)
+				{
+					for (int j = 0; j < 4; j++)
+					{
+						m_cubePoints[i][j] = yRotation * m_cubePoints[i][j];
+					}
+				}
+
+			}
+			
+			break;
+		case sf::Keyboard::Y:
+			yRotation = MyMatrix::rotationAntiClockwiseY(-rotation);
+
+			for (int i = 0; i < 6; i++)
+			{
+				MyVector3 originVector{ MyVector3{0,0,0} - m_pyramidPoints[i] };
+				MyVector3 returningVector{ m_pyramidPoints[i] };
+
+				MyMatrix translateToOrigin = MyMatrix::translationXY(originVector);
+				MyMatrix translateBack = MyMatrix::translationXY(returningVector);
+
+				
+				
+
+				for (int i = 0; i < 6; i++)
+				{
+					for (int j = 0; j < 4; j++)
+					{
+						m_cubePoints[i][j] = yRotation * m_cubePoints[i][j];
+					}
+				}
+				
+			}
+			break;
+
+		case sf::Keyboard::G:
+			xRotation = MyMatrix::rotationAntiClockwiseX(rotation);
+
+			for (int i = 0; i < 6; i++)
+			{
+				MyVector3 originVector{ MyVector3{0,0,0} - m_pyramidPoints[i] };
+				MyVector3 returningVector{ m_pyramidPoints[i] };
+
+				MyMatrix translateToOrigin = MyMatrix::translationXY(originVector);
+				MyMatrix translateBack = MyMatrix::translationXY(returningVector);
+
+
+				/*m_pyramidPoints[i] = xRotation * m_pyramidPoints[i];*/
+
+				for (int i = 0; i < 6; i++)
+				{
+					for (int j = 0; j < 4; j++)
+					{
+						m_cubePoints[i][j] = xRotation * m_cubePoints[i][j];
+					}
+				}
+
+			}
+			break;
+
+		case sf::Keyboard::H:
+			xRotation = MyMatrix::rotationAntiClockwiseX(-rotation);
+
+			for (int i = 0; i < 6; i++)
+			{
+				MyVector3 originVector{ MyVector3{0,0,0} - m_pyramidPoints[i] };
+				MyVector3 returningVector{ m_pyramidPoints[i] };
+
+				MyMatrix translateToOrigin = MyMatrix::translationXY(originVector);
+				MyMatrix translateBack = MyMatrix::translationXY(returningVector);
+
+
+				//m_pyramidPoints[i] = xRotation * m_pyramidPoints[i];
+
+				for (int i = 0; i < 6; i++)
+				{
+					for (int j = 0; j < 4; j++)
+					{
+						m_cubePoints[i][j] = xRotation * m_cubePoints[i][j];
+					}
+				}
+
+			}
+		case sf::Keyboard::V:
+			zRotation = MyMatrix::rotationAntiClockwiseZ(rotation);
+
+			for (int i = 0; i < 6; i++)
+			{
+				MyVector3 originVector{ MyVector3{0,0,0} - m_pyramidPoints[i] };
+				MyVector3 returningVector{ m_pyramidPoints[i] };
+
+				MyMatrix translateToOrigin = MyMatrix::translationXY(originVector);
+				MyMatrix translateBack = MyMatrix::translationXY(returningVector);
+
+
+				//m_pyramidPoints[i] = zRotation * m_pyramidPoints[i];
+
+				for (int i = 0; i < 6; i++)
+				{
+					for (int j = 0; j < 4; j++)
+					{
+						m_cubePoints[i][j] = zRotation * m_cubePoints[i][j];
+					}
+				}
+
+			}
+		case sf::Keyboard::B:
+			zRotation = MyMatrix::rotationAntiClockwiseZ(-rotation);
+
+			for (int i = 0; i < 6; i++)
+			{
+				MyVector3 originVector{ MyVector3{0,0,0} - m_pyramidPoints[i] };
+				MyVector3 returningVector{ m_pyramidPoints[i] };
+
+				MyMatrix translateToOrigin = MyMatrix::translationXY(originVector);
+				MyMatrix translateBack = MyMatrix::translationXY(returningVector);
+
+
+				//m_pyramidPoints[i] = zRotation * m_pyramidPoints[i];
+
+				for (int i = 0; i < 6; i++)
+				{
+					for (int j = 0; j < 4; j++)
+					{
+						m_cubePoints[i][j] = zRotation * m_cubePoints[i][j];
+					}
+				}
+
+			}
+			break;
+		case sf::Keyboard::W:
+			moveVector = { 0,0.1,0 };					
+			break;
+
+		case sf::Keyboard::A:
+			moveVector = { -0.1,0,0 };
+			break;
+
+		case sf::Keyboard::S:
+			moveVector = { 0,-0.1,0 };
+			break;
+
+		case sf::Keyboard::D:
+			moveVector = { 0.1,0,0 };
+			break;
+
+		default:
+			break;
+		}
+
+		/*for (int i = 0; i < 6; i++)
+		{
+			m_pyramidPoints[i] += moveVector;
+		}*/
+
+		for (int i = 0; i < 6; i++)
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				m_cubePoints[i][j] += moveVector;
+			}
+		}
+	}
+}
+
+void Game::unload()
+{
+	cout << "Cleaning up" << endl;
+}
+
---
+++ OpenGL3/SFMLOpenGL/Game.h
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
+#include <gl/GL.h>
+#include <gl/GLU.h>
+
+#include "MyVector3.h"
+#include "Matrix.h"
+
+using namespace std;
+using namespace sf;
+
+
+
+class Game
+{
+public:
+	Game();
+	~Game();
+	void run();
+private:
+	Window window;
+	bool isRunning = false;
+	void initialize();
+	void update();
+	void draw();
+	void processEvents(Event event);
+	void unload();
+
+	float rotation = 1.f;
+
+	MyVector3 m_cubePoints[6][4]
+	{
+		{//left
+			{0,0,-5},
+			{1,0,-5},
+			{1,0,-7},
+			{0,0,-7}
+		},
+		{//front
+			{1,0,-5},
+			{1,1,-5},
+			{1,1,-7},
+			{1,0,-7}
+		},
+		{//right
+			{1,1,-5},
+			{0,1,-5},
+			{0,1,-7},
+			{1,1,-7}
+		},
+		{//back
+			{0,1,-5},
+			{0,0,-5},
+			{0,0,-7},
+			{0,1,-7}
+		},
+		{//top
+			{1,0,-7},
+			{1,1,-7},
+			{0,1,-7},
+			{0,0,-7}
+		},
+		{//bottom
+			{1,0,-5},
+			{1,1,-5},
+			{0,1,-5},
+			{0,0,-5}
+		},
+	};
+
+	MyVector3 m_cubeColors[4]
+	{
+		{1.0f, 0.0f, 0.0f},//V0(red)
+		{0.0f, 1.0f, 0.0f}, //V1(green)
+		{0.0f, 0.0f, 1.0f}, //V2(blue)
+		{0.0f, 1.0f, 0.0f}, //V3(green)
+	};
+
+	MyVector3 m_pyramidPoints[6]
+	{
+		{0.0f, 1.0f, -15.0f},
+		{-1.0f, -1.0f, -14.0f},
+		{1.0f, -1.0f, -14.0f},
+		{1.0f, -1.0f, -16.0f},
+		{-1.0f, -1.0f, -16.0f},
+		{-1.0f, -1.0f, -14.0f}
+	};
+
+	MyVector3 m_pyramidColors[6]
+	{
+		{1.0f, 0.0f, 0.0f},//V0(red)
+		{0.0f, 1.0f, 0.0f}, //V1(green)
+		{0.0f, 0.0f, 1.0f}, //V2(blue)
+		{0.0f, 1.0f, 0.0f}, //V3(green)
+		{0.0f, 0.0f, 1.0f}, //V4(blue)
+		{0.0f, 1.0f, 0.0f} //V5(green)
+	};
+
+	//midpoint of pyramid which is the base point
+	MyVector3 baseOfPyramid;
+	MyVector3 centroidOfPyramid;
+
+	float rotationAngle = 0.005f;
+	 // ********************
+};
